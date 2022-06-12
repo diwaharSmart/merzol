acce5778c2969c0f9ebc5fe998a9263ff86e329d
diwaharSmart <smartdiwahar@outlook.com>
2022-06-12 22:29:24 +0530

    update

--- Account/models.py
+++ Account/models.py
@@ -12,7 +12,7 @@ class User(AbstractUser):
     mobile_number  = models.CharField(max_length=255,blank=True,null=True,unique=True)
     user_id        = models.CharField(default=uuid.uuid4,max_length=255,unique=True)
     country_code   = models.CharField(max_length=255,blank=True,null=True)
-    profile        = models.ImageField(upload_to = "user/profile/images/")
+    profile        = models.ImageField(upload_to = "user/profile/images/",blank=True,null=True)
     online         = models.BooleanField(default=False)
     last_seen      = models.CharField(max_length=255,blank=True,null=True)
 
